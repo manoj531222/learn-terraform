2416164cfbe0f5f93e4e04351f5d6c0a8ed9b826
manoj <gandhamanojkutty@gmail.com>
2023-04-29 17:23:56 +0530

    var.tf

--- 01-vars/var.tf
+++ 01-vars/var.tf
@@ -1,11 +1,3 @@
 variable "sample_string" {
   default = "hello world"
 }
-
-output "sample_string" {
-  value = var.sample_string
-}
-
-output "sample_string1" {
-  value = "Value of sample_string = ${var.sample_string}"
-}
