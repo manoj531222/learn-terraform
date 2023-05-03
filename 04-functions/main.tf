ba603ad918c30b8d5b0f91bc7e58db5247801d6f
manoj <gandhamanojkutty@gmail.com>
2023-05-03 08:42:11 +0530

    functions

--- 04-functions/main.tf
+++ 04-functions/main.tf
@@ -15,3 +15,13 @@ output "sample1" {
   value = element(var.sample1, 0)
 }
 
+variable "sample2" {
+  default = {
+    xyz = 100
+    abc = 200
+  }
+}
+output "sample2" {
+  value = lookup(var.sample2, "a1", "dummy")
+}
+
