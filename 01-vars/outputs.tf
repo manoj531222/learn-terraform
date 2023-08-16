cb1f7d3e21ef956e7c43e213bb954fd157bdc6be
manoj <gandhamanojkutty@gmail.com>
2023-08-16 18:06:44 +0530

    var

--- 01-vars/outputs.tf
+++ 01-vars/outputs.tf
@@ -1,3 +1,6 @@
 output "name" {
   value = var.name
 }
+output "fruit" {
+  value = var.fruit
+}
--- 01-vars/vars.tf
+++ 01-vars/vars.tf
@@ -1,3 +1,7 @@
 variable "name" {
   default = "manoj"
 }
+
+variable "fruit" {
+  default = mango
+}
