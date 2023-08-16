0c0c5a706113cd8ff064307956fb7fb9c1528b04
manoj <gandhamanojkutty@gmail.com>
2023-08-16 13:12:20 +0530

    terraform

--- ex1/sample.tf
+++ ex1/sample.tf
@@ -1,8 +1,8 @@
-resource "aws_instance" "web" {
+resource "aws_instance" "test" {
   ami           = "ami-03265a0778a880afb"
   instance_type = "t3.micro"
 
   tags = {
-    Name = "HelloWorld"
+    Name = "Hello"
   }
 }
