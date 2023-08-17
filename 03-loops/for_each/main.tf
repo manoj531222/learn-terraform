cd27f95945ef995d8e8406238d77dda0efd6a4a5
manoj <gandhamanojkutty@gmail.com>
2023-08-17 14:05:36 +0530

    for_each

--- 03-loops/for_each/main.tf
+++ 03-loops/for_each/main.tf
@@ -1,15 +1,41 @@
-resource "null_resource" "marketing" {
-  for_each = var.fruits
+#resource "null_resource" "marketing" {
+#  for_each = var.fruits
+#  provisioner "local-exec" {
+#    command = "echo fruit name - ${each.key} - ${each.value}"
+#  }
+#}
+#
+#variable "fruits" {
+#  default = {
+#    apple = 100
+#    banana = 200
+#    orange = 300
+#  }
+#
+#}
+
+resource "null_resource" "city_names" {
+  for_each = var.city_names
   provisioner "local-exec" {
-    command = "echo fruit name - ${each.key} - ${each.value}"
+    command = "echo city_names - ${each.value} - ${each.key}"
   }
 }
 
-variable "fruits" {
+variable "city_names" {
   default = {
-    apple = 100
-    banana = 200
-    orange = 300
+    telangana {
+      distics = 10
+      subdistics = 100
   }
+    maharastra {
+      distics = 20
+      subdistics = 200
+    }
+    pune {
+      distics = 30
+      subdistics = 300
+}
+
+}
 
 }
